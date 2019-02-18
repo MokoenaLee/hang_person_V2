332fe5e25c562167880707b04d6898b557fcb22e
MokoenaLee <palesa.mokoena@coloradocollege.edu>
2019-02-17 23:08:35 -0700

    all tests for guessing work. Repo updated

---
+++ app.rb
@@ -0,0 +1,89 @@
+require 'sinatra/base'
+require 'sinatra/flash'
+require './lib/hangperson_game.rb'
+
+class HangpersonApp < Sinatra::Base
+
+  enable :sessions
+  register Sinatra::Flash
+
+  before do
+    @game = session[:game] || HangpersonGame.new('')
+  end
+
+  after do
+    session[:game] = @game
+  end
+
+  # These two routes are good examples of Sinatra syntax
+  # to help you with the rest of the assignment
+  get '/' do
+    redirect '/new'
+  end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/create' do
+    word = params[:word] || HangpersonGame.get_random_word
+    @game = HangpersonGame.new(word)
+    redirect '/show'
+  end
+
+  # Use existing methods in HangpersonGame to process a guess.
+  # If a guess is repeated, set flash[:message] to "You have already used that letter."
+  # If a guess is invalid, set flash[:message] to "Invalid guess."
+  
+  post '/guess' do
+     letter = params[:guess].to_s[0]
+
+     @guess = @game.guess(letter)
+
+     if @guess == false
+       flash[:message] ="Invalid guess."
+       @w_guesses = @game.guesses_so_w
+     else
+      flash[:message] = "correct guesses so far"
+      @r_guesses = @game.guesses_so_r     
+     end
+
+    redirect '/show'
+  end
+
+  
+
+  # Everytime a guess is made, we should eventually end up at this route.
+  # Use existing methods in HangpersonGame to check if player has
+  # won, lost, or neither, and take the appropriate action.
+  # Notice that the show.erb template expects to use the instance variables
+  # wrong_guesses and word_with_guesses from @game.
+  get '/show' do
+     if @game.check_win_or_lose == :win
+       flash[:message] = "CONGRATULATIONS, you've won the game!"
+       @win = @game.word_with_guesses
+     elsif @game.check_win_or_lose == :lose
+       flash[:message] = "You Lost. Restart to try again"
+       @lost = @game.word_with_guesses
+     else
+       flash[:message] = "Play On"
+       redirect '/guess'
+     end
+    
+    #erb :show # You may change/remove this line
+  end
+
+  get '/win' do
+    if @game.check_win_or_lose == :lose
+      flash[:message] = "You're a winner!"
+    end
+  end
+
+  get '/lose' do
+     if @game.check_win_or_lose == :lose
+         erb :lose
+     end
+    # You may change/remove this line
+  end
+
+end
---
+++ lib/hangperson_game.rb
@@ -0,0 +1,117 @@
+class HangpersonGame
+   attr_accessor :word
+   attr_accessor :guesses
+   attr_accessor :wrong_guesses
+   attr_accessor :w_result
+   
+   
+    
+  
+  def initialize(word)
+    @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @@guesses_right_far = []
+    @@guesses_wrong_far = []
+  
+    
+  end
+  
+  
+
+ 
+ #keeping list of correct and incorrect guesses so far by adding characters to the list
+ #ensure that if user enters more than one character variables remain unchanged
+#also ensure that character has not been guessed before
+#the return booleans are confirming whether or not the appropriate variable was changed
+
+ def guess(char_input)
+  
+  
+  if (char_input == '') || (char_input =~ /\W/) || (char_input == nil)
+    raise ArgumentError
+  end
+ 
+    char_input.downcase!
+
+
+    if @word.include?(char_input) && char_input.length ==1 && !(@@guesses_right_far.include?(char_input))
+      @@guesses_right_far << char_input
+      @guesses = char_input
+      @wrong_guesses = ''
+      return true
+    elsif @@guesses_right_far.include?(char_input) 
+         @guesses = char_input
+         @wrong_guesses = ''
+       return false
+    elsif @@guesses_wrong_far.include?(char_input)
+        @wrong_guesses = char_input
+        @guesses = ''
+        return false 
+    else
+       @@guesses_wrong_far << char_input
+       @wrong_guesses = char_input
+       @guesses = ''
+        
+       return true
+   end 
+      
+ end
+      
+ def self.guesses_so_r
+    puts "Right guess #{@@guesses_right_far}"
+ end 
+      
+ def self.guesses_so_w
+    puts "Wrong guess #{@@guesses_wrong_far}" 
+ end
+     
+#method to display the words guessed at each stage
+  def word_with_guesses
+    w_hold = @word
+    word_array= @word.chars
+    guess_right= @@guesses_right_far
+    s_out  = "" 
+    indices = []
+    word_array.each do |x|
+    indez = w_hold.index(x)
+     
+     indices.push(indez)
+     w_hold[indez] = "/"
+     
+     if (guess_right.include?(x))
+         s_out.insert(indez,x)
+     elsif  indices.include?(indez)
+          s_out.insert(indez,"-")
+      else
+          s_out.insert(indez,"-")   
+     end
+    end
+    return s_out
+  end
+     
+
+#method to determine whether the player wins or loses
+ def check_win_or_lose()
+    if @@guesses_right_far.length == @word.length
+       return :win
+    elsif @@guesses_wrong_far.length >=7
+       return :lose
+    else
+       return :play
+    end
+    
+ end
+
+ #Service-Oriented-Architecture...random word generator used for each round of the game
+
+  def self.get_random_word
+    require 'uri'
+    require 'net/http'
+    uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
+    Net::HTTP.new('watchout4snakes.com').start { |http|
+      return http.post(uri, "").body
+    }
+  end
+
+end
